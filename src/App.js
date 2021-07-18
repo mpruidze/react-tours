1bdabf39c45c43fec885d38ff218b600bae9a766
Medea Pruidze <medeapruidze3@gmail.com>
2021-07-18 18:30:43 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Tours from './components/Tours';
 import Tour from './components/Tour';
 import Loading from './components/Loading';
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const resp = await fetch(url);
+      const tours = await resp.json();
+      setLoading(false);
+      setTours(tours);
+      console.log(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  },[]);
+
   if (loading) {
     return (
       <main>
@@ -18,9 +39,17 @@ function App() {
       </main>
     )
   }
+  if(tours.length==0){
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button  className='btn' onClick={() => getData()}>refresh</button>
+      </div>
+    </main>
+  }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
   )
 }
